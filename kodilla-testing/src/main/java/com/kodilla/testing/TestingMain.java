7acbb67f4c4e81792c4d140ff2b3f1d56f620826
Michal Kudyk <kudykmichal@gmail.com>
2021-11-22 20:13:01 +0100

    6.2  kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,22 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
+
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        if(Calculator.add(5, 3) == 5 + 3){
+            System.out.println("test add ok");
+        }else{
+            System.out.println("error add");
+        }
+
+        if(Calculator.subtract(5, 3) == 5 - 3){
+            System.out.println("test subtract ok");
+        }else{
+            System.out.println("error subtract");
+        }
     }
 }
